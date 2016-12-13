e3548d95030bfa753c653f943a9bced55c8edee1
Vaclav Dedik <vdedik@redhat.com>
2016-12-13 14:17:59 +0100

    Fix jdg-release.sh to work with ETT process

--- jdg-release.sh
+++ jdg-release.sh
@@ -18,10 +18,6 @@ DATE=`date +"%a %b %d %Y"`
 
 function prepare-src() {
 	echo "Preparing release for milestone $1"
-    git fetch -p $JDG_REMOTE
-	git branch -D $TEMP_BRANCH 2> /dev/null
-	git checkout -b $TEMP_BRANCH $JDG_REMOTE/$JDG_BRANCH
-	sed -i "s/-SNAPSHOT/.$1/" CMakeLists.txt
 	rm -fr build
 	mkdir build
 	pushd build 2> /dev/null
@@ -31,7 +27,6 @@ function prepare-src() {
 }
 
 function prepare-dist-git() {
-    CHANGELOG="$DATE $FULLNAME <${USERNAME}@redhat.com> - $VERSION.$1-$RELEASE\n- $VERSION.$1"
     rhpkg clone $DIST_GIT_REPO
     pushd $DIST_GIT_REPO
     for i in $BRANCHES;
@@ -41,19 +36,11 @@ function prepare-dist-git() {
 
             # Update .spec file
             PREV_VERSION=`grep "Version:" infinispan-cpp-client.spec | sed -e s/"Version:\s*"//`
-            PREV_RELEASE=`grep "Release:" infinispan-cpp-client.spec | sed -e s/"Release:\s*"// | sed -e s/%{?dist}//` 
  
             if [ $PREV_VERSION != $VERSION.$1 ]; then
                 sed -i "s/Version:\s*$PREV_VERSION/Version:\t$VERSION.$1/" infinispan-cpp-client.spec
-                RELEASE=1
-            else
-                RELEASE=$((PREV_RELEASE+1))
             fi
-            sed -i "s/Release:\s*$PREV_RELEASE%{?dist}/Release:\t$RELEASE%{?dist}/" infinispan-cpp-client.spec
              
-            CHANGELOG="$DATE $FULLNAME <${USERNAME}@redhat.com> - $VERSION.$1-$RELEASE\n- $VERSION.$1"
-            sed -i "s/%changelog/%changelog\n\* $CHANGELOG\n/" infinispan-cpp-client.spec
-
             # Update sources
             cp ../build/infinispan-hotrod-cpp-$VERSION.$1-Source.zip .
             rhpkg new-sources infinispan-hotrod-cpp-$VERSION.$1-Source.zip
@@ -69,38 +56,6 @@ function prepare-dist-git() {
     popd
 }
 
-function perform-dist-git() {
-    if [ ! -f $DIST_GIT_REPO ]; then
-        rhpkg clone $DIST_GIT_REPO
-    fi
-    pushd $DIST_GIT_REPO
-
-    for i in $BRANCHES;
-    do
-        git checkout $i
-        if [ $1 ]; then
-            rhpkg scratch-build
-            popd
-            exit
-        else
-            rhpkg build
-        fi
-    done
-    popd
-}
-
-function perform-src() {
-    RELEASE=`grep ::VERSION build/Version.cpp | cut -d\" -f2`
-    echo "Performing release $RELEASE"
-    git add CMakeLists.txt
-    git commit -a -m "$RELEASE"
-    git tag -a $RELEASE -m $RELEASE
-    git checkout $JDG_BRANCH
-    git branch -D $TEMP_BRANCH
-    echo "Don't forget to push the $RELEASE tag to the $REDHAT_REMOTE remote"
-    exit
-}
-
 function usage() {
 	echo "Usage: $0 [--prepare MILESTONE|--perform]"
 	exit 1
@@ -114,10 +69,6 @@ case "$1" in
         prepare-src "$2"
         prepare-dist-git "$2" "$3"
         ;;
-    "--perform" )
-        perform-dist-git "$2"
-        perform-src
-        ;;
     "--prepare-src" )
     	if [ "$#" -lt 2 ]; then
 	    usage
@@ -129,12 +80,6 @@ case "$1" in
 	    usage
 	fi
         prepare-dist-git "$2" "$3"
-        ;;
-    "--perform-dist-git" )
-        perform-dist-git "$2"
-        ;;
-    "--perform-src" )
-        perform-src
         ;;
     *)
         usage
