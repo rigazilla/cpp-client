167eb405f14fef56e2bff18cd2a85c4556bdced9
Vittorio Rigamonti <rigazilla@gmail.com>
2017-04-04 10:59:21 +0200

    windows plain and md5

--- include/infinispan/hotrod/AuthenticationConfiguration.h
+++ include/infinispan/hotrod/AuthenticationConfiguration.h
@@ -7,7 +7,39 @@
 
 #ifndef INCLUDE_INFINISPAN_HOTROD_AUTHENTICATIONCONFIGURATION_H_
 #define INCLUDE_INFINISPAN_HOTROD_AUTHENTICATIONCONFIGURATION_H_
+#if !defined _WIN32 && !defined _WIN64
 #include <sasl/sasl.h>
+#define HOTROD_SASL_CB_USER SASL_CB_USER
+#define HOTROD_SASL_CB_AUTHNAME SASL_CB_AUTHNAME
+#define HOTROD_SASL_CB_PASS SASL_CB_PASS
+#define HOTROD_SASL_CB_GETREALM SASL_CB_GETREALM 
+#define HOTROD_SASL_CB_GETPATH SASL_CB_GETPATH
+#define HOTROD_SASL_CB_LIST_END SASL_CB_LIST_END
+#else
+#define HOTROD_SASL_CB_USER 0x4001
+#define HOTROD_SASL_CB_AUTHNAME 0x4002
+#define HOTROD_SASL_CB_PASS 0x4004
+#define HOTROD_SASL_CB_GETREALM 0x4008 
+#define HOTROD_SASL_CB_GETPATH 0x0003
+#define HOTROD_SASL_CB_LIST_END 0x0000
+typedef int(*sasl_callback_ft)(void *context, int id, const char **result, unsigned *len);
+typedef struct sasl_callback {
+    /* Identifies the type of the callback function.
+    * Mechanisms must ignore callbacks with id's they don't recognize.
+    */
+    unsigned long id;
+    sasl_callback_ft proc;   /* Callback function.  Types of arguments vary by 'id' */
+    void *context;
+} sasl_callback_t;
+struct sasl_conn;
+typedef struct sasl_conn sasl_conn_t;
+typedef struct sasl_secret {
+    unsigned long len;
+    unsigned char data[1];		/* variable sized */
+} sasl_secret_t;
+#define SASL_OK          0   /* successful result */
+#define SASL_BADPARAM   -7
+#endif
 namespace infinispan {
 namespace hotrod {
 
--- src/hotrod/impl/transport/tcp/TransportObjectFactory.cpp
+++ src/hotrod/impl/transport/tcp/TransportObjectFactory.cpp
@@ -5,8 +5,20 @@
 #include "hotrod/impl/transport/tcp/TcpTransportFactory.h"
 #include "hotrod/impl/operations/AuthOperation.h"
 #include "hotrod/impl/operations/AuthMechListOperation.h"
+#if !defined _WIN32 && !defined _WIN64
 #include <sasl/sasl.h>
 #include <sasl/saslplug.h>
+#else
+#define SECURITY_WIN32
+#define IO_BUFFER_SIZE  0x10000
+#include <winsock.h>
+#include <wincrypt.h>
+#include <wintrust.h>
+#include <schannel.h>
+#include <security.h>
+#include <sspi.h>
+
+#endif
 #include <string.h>
 namespace infinispan {
 namespace hotrod {
@@ -16,6 +28,7 @@ using namespace operations;
 
 namespace transport {
 
+#if !defined _WIN32 && !defined _WIN64
 void saslfail(int why, const char *what)
 {
     // TODO: error handling
@@ -23,21 +36,39 @@ void saslfail(int why, const char *what)
     s.append(std::to_string(why)).append(" : ").append(what);
     throw HotRodClientException(s);
 }
-
+#endif
 TransportObjectFactory::TransportObjectFactory(Codec& c, TcpTransportFactory& factory) :
         tcpTransportFactory(factory), codec(c)  {
     /* callbacks we support */
     int r;
     /* initialize the sasl library */
     /* Some tests don't create a tcpTransportFactory */
+#if !defined _WIN32 && !defined _WIN64
     const sasl_callback_t *callbacks =
             tcpTransportFactory.getConfiguration().getSecurityConfiguration().getAuthenticationConfiguration().getCallbackHandler().data();
     r = sasl_client_init(callbacks);
     if (r != SASL_OK)
         saslfail(r, "initializing libsasl");
+#endif
+}
+
+void logAndThrow(const std::string& host, const int port, const std::string& msg) {
+    DEBUG(msg.c_str());
+    throw TransportException(host, port, msg, -1);
+}
+
+const sasl_callback_t* get_auth_callback(unsigned long id, const AuthenticationConfiguration& conf)
+{
+    for each (auto &var in conf.getCallbackHandler())
+    {
+        if (var.id == id)
+            return &var;
+    };
+    return nullptr;
 }
 
 void do_sasl_authentication(Codec& codec, Transport& t, const AuthenticationConfiguration& conf) {
+#if !defined _WIN32 && !defined _WIN64
     sasl_conn_t *conn;
     int r;
     const char *data;
@@ -70,6 +101,123 @@ void do_sasl_authentication(Codec& codec, Transport& t, const AuthenticationConf
     if (r != SASL_OK && r != SASL_CONTINUE) {
         saslfail(r, "performing SASL negotiation");
     }
+#else
+    SecBufferDesc   OutBuffer, InBuffer;
+    SecBuffer       InBuffers[2], OutBuffers[1];
+    DWORD           dwSSPIFlags, dwSSPIOutFlags, cbData, cbIoBuffer;
+    TimeStamp       tsExpiry;
+    SECURITY_STATUS scRet;
+    static UCHAR IoBuffer[IO_BUFFER_SIZE];
+    SEC_CHAR name[] = "localhost";
+
+    TimeStamp timestamp;
+    // Call InitializeSecurityContext.
+    CredHandle        hCred;
+    CtxtHandle   *hContext, *hContextNext;
+    SOCKET            Client_Socket;
+
+    SECURITY_STATUS   ss;
+    TimeStamp         Lifetime;
+    SecBuffer         OutSecBuff;
+    SecBufferDesc     InBuffDesc;
+    SecBuffer         InSecBuff;
+    ULONG             ContextAttributes;
+
+    ULONG cPackages = 0; PSecPkgInfo pInfo = NULL;
+    SECURITY_STATUS stat = EnumerateSecurityPackages(&cPackages, &pInfo);
+    SEC_WINNT_AUTH_IDENTITY credentials;
+    memset(&credentials, '\0', sizeof(SEC_WINNT_AUTH_IDENTITY));
+    credentials.Flags = SEC_WINNT_AUTH_IDENTITY_ANSI;
+    const char *username;
+    unsigned int userLen;
+    const char *password;
+    unsigned int passLen;
+    const char *realm;
+    unsigned int realmLen;
+    auto sasl_cb = get_auth_callback(HOTROD_SASL_CB_USER, conf);
+    sasl_cb->proc(sasl_cb->context, sasl_cb->id, &username, &userLen);
+    sasl_cb = get_auth_callback(HOTROD_SASL_CB_PASS, conf);
+    sasl_cb->proc(sasl_cb->context, sasl_cb->id, &password, &passLen);
+    sasl_cb = get_auth_callback(HOTROD_SASL_CB_GETREALM, conf);
+    sasl_cb->proc(sasl_cb->context, sasl_cb->id, &realm, &realmLen);
+    credentials.User = (unsigned char*)username;
+    credentials.UserLength = userLen;
+    credentials.Password = (unsigned char*)password;
+    credentials.PasswordLength = passLen;
+    credentials.Domain = (unsigned char*)realm;
+    credentials.DomainLength = realmLen;
+    ss = AcquireCredentialsHandle(NULL, "WDigest", SECPKG_CRED_OUTBOUND, NULL, &credentials, NULL, NULL, &hCred, &tsExpiry);
+    OutBuffers[0].pvBuffer = NULL;
+    OutBuffers[0].BufferType = SECBUFFER_TOKEN;
+    OutBuffers[0].cbBuffer = 0;
+
+    OutBuffer.cBuffers = 1;
+    OutBuffer.pBuffers = OutBuffers;
+    OutBuffer.ulVersion = SECBUFFER_VERSION;
+
+    InBuffers[0].pvBuffer = IoBuffer;
+    InBuffers[0].cbBuffer = 0x08;
+    InBuffers[0].BufferType = SECBUFFER_TOKEN;
+    std::string targetName("hotrod/");
+    targetName.append(conf.getServerFqdn());
+    hContext = new CtxtHandle();
+    memset(hContext, 0, sizeof(*hContext));
+    scRet = InitializeSecurityContext(&hCred,
+        NULL,
+        &targetName[0],
+        ISC_REQ_ALLOCATE_MEMORY,
+        0,
+        SECURITY_NATIVE_DREP,
+        NULL,
+        0,
+        hContext,
+        &OutBuffer,
+        &dwSSPIOutFlags,
+        &tsExpiry);
+    const char* choosenmech = conf.getSaslMechanism().data();
+    std::vector<char> mech(choosenmech, choosenmech + strlen(choosenmech)), resp;
+    if (!strcmp("PLAIN", choosenmech))
+    {
+        resp.insert(resp.end(), username, username + userLen + 1);
+        resp.insert(resp.end(), username, username + userLen + 1);
+        resp.insert(resp.end(), password, password + passLen);
+    }
+    AuthOperation a(codec, t, mech, resp);
+    std::vector<char> respOp(a.execute());
+    if (!strcmp("PLAIN", choosenmech))
+    {
+        return;
+    }
+    memcpy(IoBuffer, respOp.data(), respOp.size());
+    InBuffers[0].pvBuffer = IoBuffer;
+    InBuffers[0].cbBuffer = respOp.size();
+    InBuffers[0].BufferType = SECBUFFER_TOKEN;
+    InBuffer.cBuffers = 1;
+    InBuffer.pBuffers = InBuffers;
+    InBuffer.ulVersion = SECBUFFER_VERSION;
+    while (scRet == 0x90312)
+    {
+        scRet = InitializeSecurityContext(&hCred,
+            hContext,
+            &targetName[0],
+            ISC_REQ_ALLOCATE_MEMORY,
+            0,
+            SECURITY_NATIVE_DREP,
+            &InBuffer,
+            0,
+            NULL,
+            &OutBuffer,
+            &dwSSPIOutFlags,
+            &tsExpiry);
+        std::vector<char> mech(choosenmech, choosenmech + strlen(choosenmech)), resp((char*)OutBuffers[0].pvBuffer, (char*)OutBuffers[0].pvBuffer + OutBuffers[0].cbBuffer);
+        AuthOperation a(codec, t, mech, resp);
+        std::vector<char> respOp(a.execute());
+        memcpy(IoBuffer, respOp.data(), respOp.size());
+        InBuffers[0].pvBuffer = IoBuffer;
+        InBuffers[0].cbBuffer = respOp.size();
+        InBuffers[0].BufferType = SECBUFFER_TOKEN;
+    }
+#endif
 }
 
 TcpTransport& TransportObjectFactory::makeObject(const InetSocketAddress& address) {
